f6bcafaaa2518aa1158ca23630b40ae75024011a
chenchangyue <chenchangyue@163.com>
2025-06-20 09:40:32 +0800

    init

---
+++ spec/alipan_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Alipan do
+  it "has a version number" do
+    expect(Alipan::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
